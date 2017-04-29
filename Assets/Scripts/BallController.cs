dfe36e1a4be9913f42e7d8bcc94a7d14c1adf96e
Rajendra Kumar <ARR105@pitt.edu>
2017-04-29 06:57:07 -0400

    First commit

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    public float speed;
+    public Text LivesText;
+    public Text WText;
+    private Rigidbody rb;
+
+    private int lives;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        lives = 6;
+        setliveText();
+        WText.text = "";  
+    }
+
+    
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+       
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+       if(other.gameObject.CompareTag("Traps"))
+       {
+
+           
+           transform.position = new Vector3(-10.0f, 1.5f, -17.0f);
+           
+          
+           
+
+
+           lives = lives - 1;
+           rb.isKinematic = false;
+           setliveText();
+           Debug.Log("You have " + lives + " left!");
+
+           if (lives == 0)
+           {
+               Debug.Log("Game over");
+               WText.text = "Sorry, You lost!";
+               rb.isKinematic = true;
+           }
+       }
+
+    }
+
+    void setliveText()
+    {
+        LivesText.text = "Lives: " + lives.ToString();
+    }
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject Ball;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+
+        offset = transform.position - Ball.transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        transform.position = Ball.transform.position + offset;
+	}
+}
